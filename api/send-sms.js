00b52a8741cf9b0b7602dbd89a97b764f7f1b9b2
chunghu29 <65057602+chunghu29@users.noreply.github.com>
2020-12-26 21:47:12 -0800

    Log payload

--- api/send-sms.js
+++ api/send-sms.js
@@ -4,48 +4,67 @@ const multer = require('multer');
 const addrs = require("email-addresses");
 const sgMail = require('@sendgrid/mail');
 const twilio = require('twilio');
+const axios = require('axios');
+
+logPayload = (payload)=> {
+    const dest = process.env.LOG_PAYLOAD_DEST || ""
+    axios.post(dest, payload)
+    .then(res=>console.log(`Post to ${dest} success`))
+    .catch(error=>console.error(error));
+}
 
 module.exports = async (req, res) => { 
     const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-    await util.promisify(multer().any())(req, res);
+    logPayload(req.body)
 
-    const from = req.body.from;
-    const to = req.body.to;
-    const subject = req.body.subject;
-    const body = req.body.text; //req.body.html; don't think we can send html in twilio
+    process = util.promisify(multer().any())(req, res);
+    process.then(()=>{
+        const from = req.body.from;
+        const to = req.body.to;
+        const subject = req.body.subject;
+        const body = req.body.text; //req.body.html; don't think we can send html in twilio
+    
+        //Using email-addresses library to extract email details.
+        const toAddress = addrs.parseOneAddress(to);
+        const toName = toAddress.local;
+        const fromAddress = addrs.parseOneAddress(from);
+        const fromName = fromAddress.local;
 
-    //Using email-addresses library to extract email details.
-    const toAddress = addrs.parseOneAddress(to);
-    const toName = toAddress.local;
-    const fromAddress = addrs.parseOneAddress(from);
-    const fromName = fromAddress.local;
-
-    //Sending SMS with Twilio Client
-    client.messages.create({
-        to: `+${toName}`,
-        from: process.env.TWILIO_PHONE_NUMBER,
-        body
-    }).then(msg => {
-        console.log(msg)
-        res.status(200).send(msg.sid);
-    }).catch(err => {
-        //If we get an error when sending the SMS email the error message back to the sender
-        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-        // Create Email
-        const email = {
-            to: fromAddress.address,
-            from: toAddress.address,
-            subject: `Error Sending SMS to ${toAddress.local}`,
-            text: `${err}\n For email from ${fromAddress.address}`,
-        };
-        //Send Email
-        sgResp = sgMail.send(email)
-            .then(response => {
-                res.status(200).send("Sent Error Email");
-            })
-            .catch(error => {
-                res.status(500);
-            });
+        logPayload({from, to, subject, body})
+    
+        //Sending SMS with Twilio Client
+        client.messages.create({
+            to: `+${toName}`,
+            from: process.env.TWILIO_PHONE_NUMBER,
+            body
+        }).then(msg => {
+            logPayload(msg);
+            console.log(msg);
+            res.status(200).send(msg.sid);
+        }).catch(err => {
+            logPayload(err);
+            //If we get an error when sending the SMS email the error message back to the sender
+            sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    
+            // Create Email
+            const email = {
+                to: fromAddress.address,
+                from: toAddress.address,
+                subject: `Error Sending SMS to ${toAddress.local}`,
+                text: `${err}\n For email from ${fromAddress.address}`,
+            };
+            //Send Email
+            sgResp = sgMail.send(email)
+                .then(response => {
+                    logPayload(response);
+                    res.status(200).send("Sent Error Email");
+                })
+                .catch(error => {
+                    logPayload(error);
+                    res.status(500);
+                });
+        });
     });
+    
+    res.status(200).send();
 };
